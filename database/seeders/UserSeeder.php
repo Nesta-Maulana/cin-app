88e721a145edb9007d9fd8cbb7d31fe6e89b9839
Nesta Maulana <nesta.maulana@sangkuriang.co.id>
2025-02-24 15:48:29 +0700

    fix: migration file

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -36,25 +36,7 @@ class UserSeeder extends Seeder
                 'username' => 'operator',
                 'email' => 'operator@kreasisawalanusantara.com',
                 'password' => $password,
-            ],
-            [
-                'name' => 'Engineer Staff',
-                'username' => 'engineer.staff',
-                'email' => 'engineer.staff@gmail.com',
-                'password' => $password,
-            ],
-            [
-                'name' => 'Marketing Staff',
-                'username' => 'marketing.staff',
-                'email' => 'marketing.staff@gmail.com',
-                'password' => $password,
-            ],
-            [
-                'name' => 'Warehouse Staff',
-                'username' => 'warehouse.staff',
-                'email' => 'warehouse.staff@gmail.com',
-                'password' => $password,
-            ],
+            ]
         ]);
 
         $superAdmin = User::find(1);
