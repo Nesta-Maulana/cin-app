aeb39047981f350e5f077a70c7cd80e6cf6447ce
Nesta Maulana <nesta.maulana@sangkuriang.co.id>
2025-03-05 14:35:56 +0700

    fix: handling redudant item need to purchase

--- app/Http/Controllers/Transaction/DeliveryOrderController.php
+++ app/Http/Controllers/Transaction/DeliveryOrderController.php
@@ -170,7 +170,7 @@ class DeliveryOrderController extends Controller
             $customer_order_id = request()->input('customer_order_id');
             $orderBy = ['id' => 'asc'];
             $with = [];
-            $scope = ['requestStatus' => [['Waiting On Process Warehouse','Partial Delivery by Warehouse']]];
+            $scope = ['requestStatus' => [['Waiting On Process Warehouse', 'Partial Delivery by Warehouse']]];
 
             $customerData = $this->customerOrderRepository->getData(
                 $scope,
@@ -195,12 +195,14 @@ class DeliveryOrderController extends Controller
                 $itemNeedToPurchase->process_status = $checkApproval['message'];
                 $itemNeedToPurchase->save();
             }
-            foreach ($customerData->itemRequests->whereIn('request_status', ['Waiting On Process Warehouse','Partial Delivery by Warehouse']) as $key => $itemRequest) {
+            foreach ($customerData->itemRequests->whereIn('request_status', ['Waiting On Process Warehouse', 'Partial Delivery by Warehouse']) as $key => $itemRequest) {
                 foreach ($itemRequest->details as $k => $data) {
-                    $itemNeedToPurchaseDetail = $this->itemNeedToPurchaseDetailRepository->create([
-                        'header_id' => $itemNeedToPurchase->id,
-                        'item_request_detail_id' => $data->id,
-                    ]);
+                    if (!$data->itemNeedToPurchaseDetail) {
+                        $itemNeedToPurchaseDetail = $this->itemNeedToPurchaseDetailRepository->create([
+                            'header_id' => $itemNeedToPurchase->id,
+                            'item_request_detail_id' => $data->id,
+                        ]);
+                    }
                 }
             }
             DB::commit();
